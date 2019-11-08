e49840184f2f0117f7b02f3dead121fa99f3d253
kimssoo199940429 <57531119+kimssoo199940429@users.noreply.github.com>
2019-11-09 00:10:49 +0900

    Chapter2

---
+++ src/chapter2/Variable1.java
@@ -0,0 +1,18 @@
+package chapter2;
+
+public class Variable1 {
+
+	//page 45 변수 선언하기
+	public static void main(String[] args) {
+
+		int level;
+		level = 1;
+		
+		int level2 = 10; //변수 선언과 동시에 값을 대입 (초기화)
+		
+		System.out.println(level);
+		System.out.println(level2);
+		
+	}
+
+}
