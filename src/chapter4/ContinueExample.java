8f7ce1b940c81113a09306e94fe2426c2b7656c5
kimssoo199940429 <57531119+kimssoo199940429@users.noreply.github.com>
2019-11-26 23:19:19 +0900

    20191126

--- src/chapter4/ContinueExample.java
+++ src/chapter4/ContinueExample.java
@@ -9,7 +9,7 @@ public class ContinueExample {
 		
 		for(num = 1; num <= 100; num++) {
 			if(num % 2 == 0) //num값이 짝수인경우
-				continue;    //이후 수행을 생략하고 num++수행
+				continue;    //이후 수행을 생략하고 num++수행 //밑에 처리로 continue가 아니라 for문의 증감식으로 continue한다.
 			total += num;    //num 값이 홀수인 경우에만 수행
 		}
 		System.out.println("1부터 100까지의 홀수의 합은 "+total+"입니다");
